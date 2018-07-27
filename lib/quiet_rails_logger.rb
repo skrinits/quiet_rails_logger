441fd186842b060c4b6a7f08c287bbbccd55a35c
skrinits <110100reg@gmail.com>
2018-07-27 23:49:48 +0300

    fixed for action mailer

--- lib/quiet_rails_logger.rb
+++ lib/quiet_rails_logger.rb
@@ -1,7 +1,7 @@
 require 'quiet_rails_logger/configuration'
 
 module QuietRailsLogger
-  class << self  
+  class << self
     def configuration
       @configuration ||= QuietRailsLogger::Configuration.new
     end
@@ -14,12 +14,14 @@ module QuietRailsLogger
   class Engine < ::Rails::Engine
     initializer 'quiet_rails_logger.skip_info_messages' do |app|
       if Rails.env.in?(QuietRailsLogger.configuration.environments)
-        ActiveSupport::Subscriber.subscribers.each do |subscriber|
-          subscriber.patterns.each do |pattern|
-            ActiveSupport::Notifications.unsubscribe(pattern)
+        ActiveSupport.on_load(:after_initialize) do
+          ActiveSupport::Subscriber.subscribers.each do |subscriber|
+            subscriber.patterns.each do |pattern|
+              ActiveSupport::Notifications.unsubscribe(pattern)
+            end
           end
         end
-        
+
         app.middleware.delete Rails::Rack::Logger
       end
     end
